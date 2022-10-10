4d4c11c3dd5d7b7aeefcd307bd489ed3681f877b
Maxim_Pavliuc <114051787+maximpavliuc@users.noreply.github.com>
2022-10-10 19:50:35 +0300

    Add files via upload

---
+++ case_1..10.pas
@@ -0,0 +1,13 @@
+﻿program nomer_desiati;
+var n: integer;
+begin
+write('Введи оценку-');
+ReadLn(n);
+case n of
+  1..4: WriteLn('неудовлетворительно');
+  5..6: WriteLn('удовлетворительно');
+  7..8: WriteLn('хорошо');
+  9..10: WriteLn('образцово');
+  else WriteLn('*** ошибка');
+end;
+end.
---
+++ summa_po3_i_5lei.pas
@@ -0,0 +1,11 @@
+﻿program nomer_10;
+const v:array [0..4] of integer=(0,2,4,1,3);
+var s:integer;
+begin
+write('Введи сумму=');
+read(s);
+if s>7 then
+    writeln('Сумма=',s) else
+    writeln('Ошибка');
+write('По 3 лея=',v[s mod 5],'По 5 лей=',(s-v[s mod 5]*3)div 5);
+end.
