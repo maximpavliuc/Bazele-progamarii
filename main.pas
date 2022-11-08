b44c62c5af479298422060a6f2b88ecbe3f15b27
Maxim_Pavliuc <114051787+maximpavliuc@users.noreply.github.com>
2022-11-08 10:46:25 +0200

    Add files via upload

--- main.pas
+++ main.pas
@@ -1,15 +1,27 @@
-program cifra;
-var s: char;
- function symbol(s:char):boolean;
- begin
- if (ord(s)>=48) and (ord(s)<=57) then
- symbol:=true
- else
- symbol:=false;
- end;
-begin
-writeln('Symbol=')
-readln(s);
-symbol(s);
-end.
-
+program tabele ;
+type Vector = array [1..10] of integer;
+var V: Vector;
+    i:byte;
+    begin
+    //Creareea vectorului manual
+    {for i:=1 to 10 do
+    begin
+    write('V[',i,']=');
+    readln(V[i]);
+    end;}
+    //Generarea valorilor
+    for:=1 to 10 do
+    begin
+    V[i]:= Random(25);
+    end;
+    //Afisarea valorilor
+    writeln('Valorile initiale sunt=');
+    for i:=1 to 10 do
+    write(V[i],' ');
+    //Afisarea numerilor impare
+    for i:=1 to 10 do
+    if V[i] mod 2<>0 then
+    begin
+    write('Valorile impare:',V[i],' ');
+    end;
+    end.
